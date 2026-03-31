82f96b2a1be56073c0f0392248c4dc42457587b1
João Santos <contatojoaosantos01@gmail.com>
2026-03-30 22:00:00 -0300

    Aula 10

---
+++ aula10/script/fundoPagina.js
@@ -0,0 +1,54 @@
+const botaoVermelho = document.getElementById('btnRed');
+const botaoVerde = document.getElementById('btnGreen');
+const botaoAzul = document.getElementById('btnBlue');
+const botaoAmarelo = document.getElementById('btnYellow');
+const botaohide = document.querySelector("#btnHide"); //Pegar por ID -> utilizar #
+const titulo = document.querySelector("#titulo");
+const botaoTema = document.querySelector("#btnTema");
+ 
+function alterarCor(cor){
+    document.body.style.backgroundColor = cor;
+ 
+}
+ 
+botaoVermelho.addEventListener('click', () => alterarCor('red'));
+ 
+botaoVerde.addEventListener('click', () => alterarCor('green'));
+ 
+botaoAzul.addEventListener('click', () => alterarCor('blue'));
+ 
+botaoAmarelo.addEventListener('click', () => alterarCor('yellow'));
+ 
+botaohide.addEventListener('click', () =>{
+    //classList.add -> adiciona uma classe ao elemento 'titulo'
+    // titulo.classList.add('invisivel');
+ 
+    //classList.toggle -> se a classe existir, ele remove
+    // se não existir, ele adiciona
+    titulo.classList.toggle('invisivel');
+ 
+    // if(botaohide.textContent == "Esconder titulo") {
+    //     botaohide.textContent = "Mostrar titulo";
+    // }else {
+    //     botaohide.textContent = "Esconder titulo";
+    // }
+ 
+    //if ternário
+    botaohide.textContent=
+    botaohide.textContent ==
+    "Esconder titulo" ?
+    "Mostrar titulo" :
+    "Esconder titulo";
+ 
+ 
+} );
+ 
+botaoTema.addEventListener('click', () => {
+    document.body.classList.toggle('temaEscouro');
+    if(botaoTema.textContent == "Tema escuro") {
+        botaoTema.textContent = "Tema claro";
+ 
+    }else {
+        botaoTema.textContent = "Tema escuro";
+    }
+});
--- aula10/script/script.js
+++ aula10/script/script.js
@@ -1,5 +1,29 @@
 console.log("Hello World")
 
-function multiplicar() {
-    
+const nome = "João"; // Recebeu valor não pode ser alterada
+let idade = "21"; // Posso Alterar
+var sobrenome = "Ferreira" // Escopo global
+
+// function multiplicar(x, y) { //função tradicional
+//   return x*y;    
+// }
+
+//Arrow function
+
+const multiplicar = (x, y) => {
+    console.log(x*y)
+    return `O resultado da multiplicação é: $(x*y)`
+}
+const raizQuadrada = (n) => Math.sqrt(n)
+
+const subtrair = (x, y) => x-y
+
+const potencia = (x,y) => x^y
+
+const dividir = (x,y) => {
+    if (y != 0) {
+        return x/y
+    }
+
+return "Denominador precisa ser diferente de 0"
 }
